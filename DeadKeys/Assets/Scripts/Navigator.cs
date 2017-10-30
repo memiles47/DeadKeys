d2b9c3d43b1b423438aa2cd1288c526558629c65
Michael E Miles <michael@miles-me.net>
2017-10-30 12:53:56 -0500

    Scripts -
    Starting the Navigator Scripts

---
+++ DeadKeys/Assets/Scripts/Navigator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigator : MonoBehaviour
+{
+    //Reference to singleton instance
+    public static Navigator ThisInstance
+    {
+        get
+        {
+            //Get or create singleton instance
+            if (mThisInstance == null)
+            {
+                GameObject GO = new GameObject("Navigator");
+                mThisInstance = GO.AddComponent<Navigator>();
+            }
+            return mThisInstance;
+        }
+        set
+        {
+            //If not null then we already have instance
+            if (mThisInstance != null)
+            {
+                //If different, then remove duplicate immediatly
+                if (mThisInstance.GetInstanceID() != value.GetInstanceID())
+                DestroyImmediate(value.gameObject);
+                return;
+            }
+            //If new, then create new singleton instance
+            mThisInstance = value;
+        }
+    }
+
+    private static Navigator mThisInstance = null;
+    private int AnimStateHash = Animator.StringToHash("NavState");
+
+    void Awake()
+    {
+        ThisInstance = this;
+        ThisAnimator = GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
+}
