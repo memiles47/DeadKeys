b2acf5552800657cf5c53e6ad8f3f5adb3deeecd
Michael E Miles <michael@miles-me.net>
2017-11-02 19:08:50 -0500

    Health
    Health Class Created and attached to player

---
+++ DeadKeys/Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0f, 100f)]
+    private float fValue = 100f;
+
+    //Events called on health change
+    public UnityEvent OnHealthExpired;
+    public UnityEvent OnHealthChanged;
+
+
+    public float Value
+    {
+        get { return fValue; }
+        set
+        {
+            fValue = value;
+            OnHealthChanged.Invoke();
+            if (fValue <= 0f)
+            {
+                OnHealthExpired.Invoke();
+            }
+        }
+    }
+}
