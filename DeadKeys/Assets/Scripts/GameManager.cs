fc96806998942db2d32fb73c3be9364d79db83a1
Michael E Miles <michael@miles-me.net>
2017-11-07 15:21:22 -0600

    Creating GameManager
    Creating the GameManager Class - Missed in the book put provided in the assets

---
+++ DeadKeys/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager mGM = null;
+
+    public static GameManager ThisInstance
+    {
+        get
+        {
+            // Get or create singleton instance
+            if (mGM == null)
+            {
+                
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
---
+++ DeadKeys/Assets/Scripts/UIScore.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class UIScore : MonoBehaviour
+{
+	public float DisplayScore;
+	private Text ThisText;
+	public float CatchUpSpeed = 1.0f;
+	public UnityEvent OnScoreChange;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		ThisText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		DisplayScore = Mathf.Lerp(DisplayScore, GameManager.ThisInstance.Score, CatchUpSpeed = Time.deltaTime);
+		ThisText.text = "Score " + Mathf.CeilToInt(DisplayScore).ToString("D6");
+	}
+}
