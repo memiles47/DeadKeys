4391c9e69baef6e0dbfa98a927027df8c6ce1b63
Michael E Miles <michael@miles-me.net>
2017-11-08 02:59:14 -0600

    Complete Gamemanager
    Completed the game manager script

--- DeadKeys/Assets/Scripts/GameManager.cs
+++ DeadKeys/Assets/Scripts/GameManager.cs
@@ -1,31 +1,47 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager mGM = null;
+    private static GameManager mGM;
+    public int Score = 0;
+    public int BonusLevel = 0;
 
     public static GameManager ThisInstance
     {
         get
         {
-            // Get or create singleton instance
+            //Get or create singleton instance
             if (mGM == null)
             {
-                
+                GameObject GO = new GameObject("GameManager");
+                mGM = GO.AddComponent<GameManager>();
             }
+
+            return mGM;
+        }
+
+        set
+        {
+            //If not null then we already have instance
+            if (mGM != null)
+            {
+                //If different, then remove duplicate immediately
+                if (mGM.GetInstanceID() != value.GetInstanceID())
+                    DestroyImmediate(value.gameObject);
+
+                return;
+            }
+
+            //If new, then create new sinleton instance
+            mGM = value;
+            DontDestroyOnLoad(mGM.gameObject);
         }
     }
 
-	// Use this for initialization
-	void Start ()
-	{
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		
-	}
+    // Use this to initialize
+    void Awake()
+    {
+        ThisInstance = this;
+    }
 }
