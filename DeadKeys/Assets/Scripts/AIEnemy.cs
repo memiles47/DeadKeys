30baf18d2880458af7742763e72feee2ef0a592a
Michael E Miles <michael@miles-me.net>
2017-11-16 14:50:21 -0600

    Enemy Creation
    Starting enemy prefab and AIEnemy.cs script

---
+++ DeadKeys/Assets/Scripts/AIEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class AIEnemy : MonoBehaviour
+{
+	public enum Aistate
+	{
+		IDLE = 0,
+		CHASE = 1,
+		ATTACK = 2,
+		DEAD = 3
+	};
+
+	[SerializeField] private Aistate _mActivateState = Aistate.IDLE;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
