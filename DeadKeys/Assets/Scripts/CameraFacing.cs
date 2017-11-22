990c4d7052f78cad2ec71b53bd9105214052faa4
Michael E Miles <michael@miles-me.net>
2017-11-22 11:22:57 -0600

    Camera Facing for text
    Text above zombie heads will face the camera regardless of the position of the zombie

---
+++ DeadKeys/Assets/Scripts/CameraFacing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFacing : MonoBehaviour
+{
+    private Transform _thisTransform;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        _thisTransform = GetComponent<Transform>();
+    }
+    
+    // Update is called once per frame
+    void LateUpdate ()
+    {
+        _thisTransform.LookAt(Camera.main.transform);
+    }
+}
