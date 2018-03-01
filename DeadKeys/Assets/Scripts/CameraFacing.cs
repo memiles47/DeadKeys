9c05727802e44fe29d2f51a0a35c51645a8ab587
Michael Miles <michael@miles-me.net>
2018-03-01 03:29:42 -0600

    Comment removal
    Removing comments deemed not necessary

--- DeadKeys/Assets/Scripts/CameraFacing.cs
+++ DeadKeys/Assets/Scripts/CameraFacing.cs
@@ -4,13 +4,11 @@ public class CameraFacing : MonoBehaviour
 {
     private Transform _thisTransform;
 
-    // Use this for initialization
     public void Awake ()
     {
         _thisTransform = GetComponent<Transform>();
     }
     
-    // Update is called once per frame
     public void LateUpdate ()
     {
         _thisTransform.LookAt(Camera.main.transform);
